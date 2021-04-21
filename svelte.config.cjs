3ef72a9832dd47509bb399bafa8cbb68f0561670
Kazuki Shibata <k.shibata97@gmail.com>
2021-04-21 22:13:37 +0900

    [update] 不要なコードを削除

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,4 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
 const vercel = require('@sveltejs/adapter-vercel');
 const pkg = require('./package.json');
 
