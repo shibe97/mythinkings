a763e4c99257b0a06035d084c4e8501812031dc4
Kazuki Shibata <k.shibata97@gmail.com>
2021-04-29 21:16:50 +0900

    [update] adapter-staticに変更

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
-const vercel = require('@sveltejs/adapter-vercel');
+//const vercel = require('@sveltejs/adapter-vercel');
+const static = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: vercel(),
+		adapter: static(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
